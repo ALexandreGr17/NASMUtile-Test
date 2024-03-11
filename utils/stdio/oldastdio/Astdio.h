4c362e1b55083e2d4463ef0edf899602369432dc
alexandre <alexandre.grandon@orange.fr>
2024-03-11 15:59:03 +0100

    supr old prinf

--- utils/stdio/oldastdio/Astdio.h
+++
@@ -1,8 +0,0 @@
-#ifndef ASTDIO_H
-#define ASTDIO_H
-
-void PrintS(char* str);
-void PrintI(int n);
-void PrintF(float f);
-void PrintC(char c);
-#endif
--- utils/stdio/oldastdio/aprintf.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-void AprintF(char* format, int nb, void* args);
